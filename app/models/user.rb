f98b2b1a3e344d8bb024d43b3686fc5ba227350a
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-08-09 22:11:01 -0700

    Updated finders in user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,18 @@ require 'digest/sha2'
 
 class User < ActiveRecord::Base
 	validates_uniqueness_of :username
-	
+
 	def self.authenticate(username, password)
-		user = User.find(:first, :conditions => ['username = ?', username])
+		user = User.first(:conditions => ['username = ?', username])
 		if user.blank? || Digest::SHA256.hexdigest(password + user.password_salt) != user.password_hash
 			raise "Invalid Username or Password."
 		end
 		user
 	end
-	
+
 	def password=(pass)
 		salt = [Array.new(6) {rand(256).chr}.join].pack("m").chomp
-		self.password_salt, self.password_hash = 
+		self.password_salt, self.password_hash =
 			salt, Digest::SHA256.hexdigest(pass + salt)
 	end
 
