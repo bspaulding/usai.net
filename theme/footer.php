2d8ae36bd480eaeb2359e60e393958884779bf32
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-10-16 09:42:02 -0700

    Added footer widget, removed hardcoded seal from footer

--- theme/footer.php
+++ theme/footer.php
@@ -10,7 +10,7 @@
 	-->
 	</script>
 	| 978 469 0682 | <a href="/legal">Legal Notice</a><br/>
-	<script type='text/javascript' src='https://www.rapidscansecure.com/siteseal/siteseal.js?code=109,DB4E71F64C02C894BC9F8161CF454B974B67072B'></script>
+	<?php get_sidebar( 'footer' ); ?>
 </div>
 <?php wp_footer(); ?>
 </body>
